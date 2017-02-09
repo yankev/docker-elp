260244c30f07d5ee3856ad2b0a42dde28b5f7894
yankev <kevinn.yan@gmail.com>
2017-02-08 19:16:51 -0500

    update the initial db

--- postgres/init.sql
+++ postgres/init.sql
@@ -8,3 +8,13 @@ INSERT INTO contacts(email, first_name, last_name) VALUES('jim@example.com', 'Ji
 INSERT INTO contacts(email, first_name, last_name) VALUES('JLIN', 'John', 'Smith');
 INSERT INTO contacts(email, first_name, last_name) VALUES('carol@example.com', 'Carol', 'Smith');
 INSERT INTO contacts(email, first_name, last_name) VALUES('sam@example.com', 'Sam', 'Walko');
+
+create table fav_colour (
+  id serial primary key,
+  uid integer REFERENCES "contacts"("uid"),
+  colour VARCHAR(10)
+);
+
+INSERT INTO fav_colour(uid, colour) VALUES (1, 'green');
+INSERT INTO fav_colour(uid, colour) VALUES (2, 'purp');
+INSERT INTO fav_colour(uid, colour) VALUES (3, 'blue');
